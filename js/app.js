067e50712a12a7448fa289ba60db957c34af50d0
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-02-17 23:11:16 +0200

    lab02 is done

---
+++ js/app.js
@@ -0,0 +1,98 @@
+'use strict'
+var greeting= prompt('what is your name please?'); 
+alert('Hello ' + greeting);
+
+var q1= prompt('Do you think that i have a passion with coding? Yes/No '); 
+switch(q1.toLowerCase()) {
+    case 'yes' :
+     case 'y':
+        console.log('Do you think that i have a passion with coding?',q1);
+        alert('You are totally right');
+        break;
+    case 'no' :
+    case 'n' :
+        console.log('Do you think that i have a passion with coding?',q1);
+        alert('Oh sorry you didnt guess it right');
+        break;
+    default:
+        console.log('Do you think that i have a passion with coding?',q1);
+}
+
+
+
+var q2= prompt('Do you think that i can write a code for 10 hours ? Yes/No '); 
+switch(q2.toLowerCase()) {
+    case 'yes' :
+     case 'y':
+        console.log('Do you think that i can write a code for 10 hours ?',q2);
+        alert('Oh sorry you didnt guess it right');
+        break;
+    case 'no' :
+    case 'n' :
+        console.log('Do you think that i can write a code for 10 hours ?',q2);
+        alert(' You are totally right');
+        break;
+    default:
+        console.log('Do you think that i can write a code for 10 hours ?',q2);
+}
+
+
+
+var q3= prompt('Do you think that my favorite color is Pink? Yes/No '); 
+switch(q3.toLowerCase()) {
+    case 'yes' :
+     case 'y':
+        console.log('Do you think that my favorite color is Pink?',q3);
+        alert('You are totally right');
+        break;
+    case 'no' :
+    case 'n' :
+        console.log('Do you think that my favorite color is Pink?',q3);
+        alert('Oh sorry you didnt guess it right');
+        break;
+    default:
+        console.log('Do you think that my favorite color is Pink?',q3);
+}
+
+
+var q4= prompt('Do you think i like workout? Yes/No '); 
+switch(q2.toLowerCase()) {
+    case 'yes' :
+     case 'y':
+        console.log('Do you think i like workout?',q4);
+        alert('You are totally right');
+        break;
+    case 'no' :
+    case 'n' :
+        console.log('Do you think i like workout?',q4);
+        alert('Oh sorry you didnt guess it right');
+        break;
+    default:
+        console.log('Do you think i like workout?',q4);
+}
+
+var q5= prompt('Do you think my favorite workout is Aerobics? Yes/No '); 
+switch(q5.toLowerCase()) {
+    case 'yes' :
+     case 'y':
+        console.log('Do you think my favorite workout is Aerobics?',q5);
+        alert('Oh sorry you didnt guess it right');
+        break;
+    case 'no' :
+    case 'n' :
+        console.log('Do you think my favorite workout is Aerobics?',q5);
+        alert('You are totally right');
+        break;
+    default:
+        console.log('Do you think my favorite workout is Aerobics?',q5);
+}
+
+
+
+
+alert('THANK YOU FOR VISITING MY PAGE '+  greeting);
+
+
+
+
+
