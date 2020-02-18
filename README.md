64300f93b8935a2a795707ea8a520f643c71a9e3
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-02-18 12:31:26 +0200

    editing my repo depend on TA's comment

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # About-me
+# Lab: “About Me” project
+- This page is showing a small  biography about me.
+-  by: Ahlam Alefishat
+
+- Tools are used: Terminal (ubuntu, VS code )
+
+## The license i have used is : MIT .
--- js/app.js
+++ js/app.js
@@ -6,16 +6,17 @@ var q1= prompt('Do you think that i have a passion with coding? Yes/No ');
 switch(q1.toLowerCase()) {
     case 'yes' :
      case 'y':
-        console.log('Do you think that i have a passion with coding?',q1);
+        //console.log('Do you think that i have a passion with coding?',q1);
         alert('You are totally right');
         break;
     case 'no' :
     case 'n' :
-        console.log('Do you think that i have a passion with coding?',q1);
+        //console.log('Do you think that i have a passion with coding?',q1);
         alert('Oh sorry you didnt guess it right');
         break;
     default:
-        console.log('Do you think that i have a passion with coding?',q1);
+        //console.log('Do you think that i have a passion with coding?',q1);
+        alert('try again')
 }
 
 
@@ -24,16 +25,17 @@ var q2= prompt('Do you think that i can write a code for 10 hours ? Yes/No ');
 switch(q2.toLowerCase()) {
     case 'yes' :
      case 'y':
-        console.log('Do you think that i can write a code for 10 hours ?',q2);
+        //console.log('Do you think that i can write a code for 10 hours ?',q2);
         alert('Oh sorry you didnt guess it right');
         break;
     case 'no' :
     case 'n' :
-        console.log('Do you think that i can write a code for 10 hours ?',q2);
+        //console.log('Do you think that i can write a code for 10 hours ?',q2);
         alert(' You are totally right');
         break;
     default:
-        console.log('Do you think that i can write a code for 10 hours ?',q2);
+        //console.log('Do you think that i can write a code for 10 hours ?',q2);
+        alert('try again')
 }
 
 
@@ -42,16 +44,17 @@ var q3= prompt('Do you think that my favorite color is Pink? Yes/No ');
 switch(q3.toLowerCase()) {
     case 'yes' :
      case 'y':
-        console.log('Do you think that my favorite color is Pink?',q3);
+        //console.log('Do you think that my favorite color is Pink?',q3);
         alert('You are totally right');
         break;
     case 'no' :
     case 'n' :
-        console.log('Do you think that my favorite color is Pink?',q3);
+        //console.log('Do you think that my favorite color is Pink?',q3);
         alert('Oh sorry you didnt guess it right');
         break;
     default:
-        console.log('Do you think that my favorite color is Pink?',q3);
+        //console.log('Do you think that my favorite color is Pink?',q3);
+        alert('try again')
 }
 
 
@@ -59,32 +62,34 @@ var q4= prompt('Do you think i like workout? Yes/No ');
 switch(q2.toLowerCase()) {
     case 'yes' :
      case 'y':
-        console.log('Do you think i like workout?',q4);
+        //console.log('Do you think i like workout?',q4);
         alert('You are totally right');
         break;
     case 'no' :
     case 'n' :
-        console.log('Do you think i like workout?',q4);
+        //console.log('Do you think i like workout?',q4);
         alert('Oh sorry you didnt guess it right');
         break;
     default:
-        console.log('Do you think i like workout?',q4);
+        //console.log('Do you think i like workout?',q4);
+        alert('try again')
 }
 
 var q5= prompt('Do you think my favorite workout is Aerobics? Yes/No '); 
 switch(q5.toLowerCase()) {
     case 'yes' :
      case 'y':
-        console.log('Do you think my favorite workout is Aerobics?',q5);
+       // console.log('Do you think my favorite workout is Aerobics?',q5);
         alert('Oh sorry you didnt guess it right');
         break;
     case 'no' :
     case 'n' :
-        console.log('Do you think my favorite workout is Aerobics?',q5);
+        //console.log('Do you think my favorite workout is Aerobics?',q5);
         alert('You are totally right');
         break;
     default:
-        console.log('Do you think my favorite workout is Aerobics?',q5);
+        //console.log('Do you think my favorite workout is Aerobics?',q5);
+        alert('try again')
 }
 
 
