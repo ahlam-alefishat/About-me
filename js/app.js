32175b682c62446025f792d1d837f8292cbbb9bf
SarahMalash <sarahabumalash@gmail.com>
2020-02-19 14:52:53 +0200

    pairing

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 'use strict'
-var score=0;
+/*var score=0;
+
 var greeting= prompt('what is your name please?'); 
 alert('Hello ' + greeting);
-
+function question1 (){
 var q1= prompt('Do you think that i have a passion with coding? Yes/No '); 
 switch(q1.toLowerCase()) {
     case 'yes' :
@@ -20,9 +21,10 @@ switch(q1.toLowerCase()) {
         //console.log('Do you think that i have a passion with coding?',q1);
         alert('try again')
 }
+}
+question1();
 
-
-
+function question2 (){
 var q2= prompt('Do you think that i can write a code for 10 hours ? Yes/No '); 
 switch(q2.toLowerCase()) {
     case 'yes' :
@@ -40,9 +42,10 @@ switch(q2.toLowerCase()) {
         //console.log('Do you think that i can write a code for 10 hours ?',q2);
         alert('try again')
 }
+}
+question2();
 
-
-
+function question3 (){
 var q3= prompt('Do you think that my favorite color is Pink? Yes/No '); 
 switch(q3.toLowerCase()) {
     case 'yes' :
@@ -60,8 +63,10 @@ switch(q3.toLowerCase()) {
         //console.log('Do you think that my favorite color is Pink?',q3);
         alert('try again')
 }
+}
+question3();
 
-
+function question4 (){
 var q4= prompt('Do you think i like workout? Yes/No '); 
 switch(q4.toLowerCase()) {
     case 'yes' :
@@ -79,7 +84,9 @@ switch(q4.toLowerCase()) {
         //console.log('Do you think i like workout?',q4);
         alert('try again')
 }
-
+}
+question4();
+function question5 (){
 var q5= prompt('Do you think my favorite workout is Aerobics? Yes/No '); 
 switch(q5.toLowerCase()) {
     case 'yes' :
@@ -97,12 +104,10 @@ switch(q5.toLowerCase()) {
         //console.log('Do you think my favorite workout is Aerobics?',q5);
         alert('try again')
 }
+}
+question5();
 
-
-
-
-
-
+function question6 (){
 var q6 = prompt('Can guess my lucky number ?');
 
 for (var i = 0; i <= 3; i++) {
@@ -127,15 +132,16 @@ for (var i = 0; i <= 3; i++) {
     }
 }
 alert('The right answer is number 4')
+} question6();
 
-
+function question7 (){
 var choices = ['action', 'comedy', 'drama', 'biography'];
 choices.push('aaa');
 for (var a = 0; a <= 6; a++) {
     if(a==6){
         alert('you ran out of attempts ');
         break;
-    }
+     }
     var q7= prompt('Come on its your turn to guess which types of movies i prefer?');
 
   for (var s=0; s <= choices.length ; s++)
@@ -145,16 +151,20 @@ for (var a = 0; a <= 6; a++) {
         score++;
         break;
     } 
+    if (q7.toLowerCase() === choices[s] ){
+        break;
+    }
     
-
 }
+}
+question7();
 
 alert('your score is '+score)
 alert('Thanks for visiting my page you are welcome any time '+  greeting);
 
 
 
-
+*/
 
 
 
